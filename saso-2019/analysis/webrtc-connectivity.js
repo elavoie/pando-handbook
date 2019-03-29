af88c1950d77e3524af1212d3ed8c3e289792aa2
Erick <github@ericklavoie.com>
2019-03-29 17:42:38 +0100

    Committed modification to analysis script

--- saso-2019/analysis/webrtc-connectivity.js
+++ saso-2019/analysis/webrtc-connectivity.js
@@ -1,15 +1,18 @@
 #!/usr/bin/env node
-
+var argv = require('minimist')(process.argv)
 var fs = require('fs')
 var data = JSON.parse(fs.readFileSync(process.argv[2]))
 
 var results = {
   nbAttempts: 0,
+  nbOpened: 0,
   nbConfirmed: 0,
   latencies: []
 }
 
 var attempts = {}
+var participants = {}
+var connected = {}
 
 for (var i = 0; i < data.length; ++i) {
   var ev = data[i]  
@@ -20,9 +23,20 @@ for (var i = 0; i < data.length; ++i) {
       attempts[ev.origin] = {}
     }
     attempts[ev.origin][ev.destination] = ev
+  } else if (ev.type === 'webrtc-connection-opened') {
+    results.nbOpened++
   } else if (ev.type === 'webrtc-connection-confirmed') {
     results.nbConfirmed++
     results.latencies.push(new Date(ev.timestamp) - new Date(attempts[ev.origin][ev.destination].timestamp))
+  } else if (ev.type === 'participant-status-updated') {
+    participants[ev.id] = {
+      latitude: ev.latitude,
+      longitude: ev.longitude
+    }
+  } else if (ev.type === 'participant-connected') {
+    connected[ev.id] = ev
+  } else if (ev.type === 'participant-disconnected') {
+    delete connected[ev.id]
   }
 }
 
@@ -41,14 +55,14 @@ for (var i = 0; i < results.latencies.length; ++i) {
 }
 
 
-var step = (max + 100) / 20.
+var step = argv['step'] ? argv['step'] : 500
 var pdf_latencies = []
 var counts = []
-for (var i = 0; i <= 20; ++i) {
-  pdf_latencies.push(i*step)
+for (var i = 0; i <= 33; ++i) {
+  pdf_latencies.push((i+1)*step)
   var count = 0
   for (var j = 0; j < results.latencies.length; ++j) {
-    if (results.latencies[j] <= i*step) {
+    if ((i)*step <= results.latencies[j] && results.latencies[j] < (i+1)*step) {
       count++
     }
   }
@@ -56,9 +70,12 @@ for (var i = 0; i <= 20; ++i) {
 }
 
 console.log('nb attempts: ' + results.nbAttempts)
+console.log('nb opened: ' + results.nbOpened)
 console.log('nb confirmed: ' + results.nbConfirmed)
 console.log('success %: ' + (results.nbConfirmed/results.nbAttempts)*100)
 console.log('min latency (ms): ' + min)
 console.log('max latency (ms): ' + max)
 console.log('latencies: ' + JSON.stringify(pdf_latencies))
 console.log('counts: ' + JSON.stringify(counts))
+console.log('nb participants: ' + Object.keys(participants).length)
+console.log('geolocation of participants: ' + JSON.stringify(participants))
